403c96766c30b877c5657b0b87023d57f44dfd11
gsvolt <gsvolt7@gmail.com>
2019-02-02 23:14:08 -0500

    code cleanup

--- notes/app.py
+++ notes/app.py
@@ -7,8 +7,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if session.get("notes") is None:
